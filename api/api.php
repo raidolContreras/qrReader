b9973220db4d3d22f18c0a1f856591c63a95396a
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2025-02-17 22:57:45 -0600

    Revert "correction to api"
    This reverts commit a6bf81f1dd91bf30bd6ac9bfe7cad51cbc9c7f93.

--- api/api.php
+++ api/api.php
@@ -1,13 +1,14 @@
 <?php
 // api.php
-require_once "../forms.controller.php";
-require_once "../../model/forms.models.php";
+require_once "../controller/forms.controller.php";
+require_once "../model/forms.models.php";
+echo "hola";
 
 header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $action = $_POST['action'] ?? '';
-
+    
     switch ($action) {
         case 'getStudentData':
             if (isset($_POST['matricula'])) {
@@ -15,21 +16,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $response = FormsController::getStudentData($matricula);
                 echo json_encode($response);
             } else {
-                echo json_encode([
-                    'error' => 'Missing matricula parameter'
-                ]);
-                die;
+                echo json_encode(["error" => "Falta el parámetro 'matricula'"]);
             }
             break;
-
+        
         default:
-            echo json_encode([
-                'error' => 'No action specified'
-            ]);
+            echo json_encode(["error" => "Acción no válida"]);
             break;
     }
-    die;
-    echo json_encode([
-        'error' => 'Invalid action'
-    ]);
+} else {
+    echo json_encode(["error" => "Solicitud no válida"]);
 }
