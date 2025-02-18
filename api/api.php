c3a01b84761e4a4d62df00efe52f102deba001fd
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2025-02-17 23:12:06 -0600

    Update api.php

--- api/api.php
+++ api/api.php
@@ -5,24 +5,20 @@ require_once "../model/forms.models.php";
 
 header("Content-Type: application/json");
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $action = $_POST['action'] ?? '';
+$action = $_POST['action'] ?? '';
+
+switch ($action) {
+    case 'getStudentData':
+        if (isset($_POST['matricula'])) {
+            $matricula = $_POST['matricula'];
+            $response = FormsController::getStudentData($matricula);
+            echo json_encode($response);
+        } else {
+            echo json_encode(["error" => "Falta el parámetro 'matricula'"]);
+        }
+        break;
     
-    switch ($action) {
-        case 'getStudentData':
-            if (isset($_POST['matricula'])) {
-                $matricula = $_POST['matricula'];
-                $response = FormsController::getStudentData($matricula);
-                echo json_encode($response);
-            } else {
-                echo json_encode(["error" => "Falta el parámetro 'matricula'"]);
-            }
-            break;
-        
-        default:
-            echo json_encode(["error" => "Acción no válida"]);
-            break;
-    }
-} else {
-    echo json_encode(["error" => "Solicitud no válida"]);
+    default:
+        echo json_encode(["error" => "Acción no válida"]);
+        break;
 }
