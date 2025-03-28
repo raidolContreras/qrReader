0d2acca6bb56663adff1f7ecc306f3767972c9d6
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2025-03-28 16:33:42 -0600

    qr white list incomplete

--- whitelist.php
+++ whitelist.php
@@ -2,23 +2,52 @@
 
 session_start();
 
-if (!isset($_SESSION['logged'])) {
-    $title = 'Iniciar sesión';
-    $pagina = filter_input(INPUT_GET, 'pagina') ?: 'login';
-    if ($pagina == 'login') {
-        include_once 'view/pages/auth/login.php';
-    } else {
-        header("Location: login");
-        exit();
-    }
+if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin') {
+    $pagina = $_GET['pagina'] ?? 'Users';
+} elseif (isset($_SESSION['role']) && $_SESSION['role'] == 'moderador') {
+    $pagina = $_GET['pagina'] ?? 'Users';
 } else {
-    $pagina = filter_input(INPUT_GET, 'pagina') ?: 'users';
-    $title = "Gestión de Usuarios";
-    includeDataAdmin($pagina);
-    // if ($_SESSION['rol'] == 'admin') {
-    // } else if ($_SESSION['rol'] == 'moderator') {
-    // } else {
-    // }
+    $pagina = $_GET['pagina'] ?? 'qrScan';
+}
+
+$userNavs = [
+    'qrScan'
+];
+
+$moderadorNavs = [
+    'Users',
+];
+
+$adminNavs = [
+    'configuration',
+    'Users'
+];
+
+if (isset($_SESSION['logged']) && $_SESSION['logged'] == true) {
+    if ($_SESSION['role'] == 'admin') {
+        if (in_array($pagina, $adminNavs)) {
+            includeData($pagina, 'admin');
+        } else {
+            includeError404();
+        }
+    } elseif ($_SESSION['role'] == 'moderador') {
+        if (in_array($pagina, $moderadorNavs)) {
+            includeData($pagina, 'moderador');
+        } else {
+            includeError404();
+        }
+    } else {
+        if (in_array($pagina, $userNavs)) {
+            includeData($pagina, 'user');
+        } else {
+            includeError404();
+        }
+    }
+} elseif ($pagina == 'Login') {
+    include "view/pages/auth/login.php";
+} else {
+    header("Location: Login");
+    exit();
 }
 
 function includeError404()
@@ -26,12 +55,12 @@ function includeError404()
     include 'error404.php';
 }
 
-function includeDataAdmin($pagina)
+function includeData($pagina, $role)
 {
     require 'view/css.php';
     require 'view/navs/navbar.php';
     require 'view/navs/sidenav.php';
-    require "view/pages/admin/$pagina.php";
+    require "view/pages/$role/$pagina.php";
     require 'view/js.php';
     echo "
     <footer>
