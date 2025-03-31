63bdd0e091cd124b02767a0de89c1b82bad79cfd
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2025-03-31 09:43:31 -0600

    Update sidenav.php

--- view/navs/sidenav.php
+++ view/navs/sidenav.php
@@ -4,6 +4,6 @@
     </div>
     <button class="btn btn-light w-100 mb-3" id="closeSidebar"><i class="fas fa-times"></i></button>
     <h4>Menú</h4>
-    <a href="Users"><i class="fas fa-users"></i> Usuarios</a>
+    <a href="users"><i class="fas fa-users"></i> Usuarios</a>
     <a href="routes"><i class="fas fa-exchange-alt"></i> Rutas</a>
 </div>
