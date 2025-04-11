598be50f328b7099d26439913a72983b3861ba10
Oscar Contreras <81644152+raidolContreras@users.noreply.github.com>
2025-04-10 19:16:04 -0600

    Update CreateScansTable.php

--- install/backend/migrations/CreateScansTable.php
+++ install/backend/migrations/CreateScansTable.php
@@ -17,7 +17,7 @@ class CreateScansTable extends AbstractMigration {
             routeScan INT(11) NOT NULL,
             dateScan TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
             KEY idUser_scan (idUser_scan),
-            KEY idRoute (idRoute),
+            KEY routeScan (routeScan),
             CONSTRAINT fk_scans_users FOREIGN KEY (idUser_scan) REFERENCES users (id) ON DELETE CASCADE,
             CONSTRAINT fk_scans_routes FOREIGN KEY (routeScan) REFERENCES routes (idRoute) ON DELETE CASCADE
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
